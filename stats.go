b245c5480fe3b315bc386f274518fd71a7c604ec
Jack Baldry <jack.baldry@grafana.com>
2022-03-28 14:07:28 +0100

    Add Niall to the stats pages
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- stats.go
+++ stats.go
@@ -118,7 +118,7 @@ func computeStats(raw map[string]RawStats) map[string]HistoricStats {
 
 	for p, stats := range raw {
 		// Filter players with an insignificant number of games.
-		if stats.Games < 3 || p == "" {
+		if (stats.Games < 3 && p != "Niall") || p == "" {
 			continue
 		}
 		computed[p] = HistoricStats{
