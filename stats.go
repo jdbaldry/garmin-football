f0d9d5894e6e319f13dcfde9db34cadbfbc645e8
Jack Baldry <jack.baldry@grafana.com>
2022-06-09 11:34:54 +0100

    Ignore "Other" player in stats
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- stats.go
+++ stats.go
@@ -118,7 +118,7 @@ func computeStats(raw map[string]RawStats) map[string]HistoricStats {
 
 	for p, stats := range raw {
 		// Filter players with an insignificant number of games.
-		if stats.Games < 2 || p == "" {
+		if stats.Games < 2 || p == "" || p == "Other" {
 			continue
 		}
 		computed[p] = HistoricStats{
